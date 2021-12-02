e42933aebe54905ba68276e8c73c52db56fe2606
pin2t <pin2t@mail.ru>
2021-12-02 08:04:02 +0500

    two parts

--- 1.go
+++ 1.go
@@ -9,19 +9,22 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	prev := [3]int64{100000000000000, 1000000000000, 10000000000000}
-	result := 0
+	prev := [3]int64{0, 0, 0}
+	result1 := 0
+	result2 := 0
 	for i := 0; i < 3 && scanner.Scan(); i++ {
 		prev[i], _ = strconv.ParseInt(scanner.Text(), 0, 0)
 	}
+	if prev[1] > prev[0] { result1++ }
+	if prev[2] > prev[1] { result1++ }
 	for scanner.Scan() {
 		value, _ := strconv.ParseInt(scanner.Text(), 0, 0)
-		if value+prev[1]+prev[2] > prev[0]+prev[1]+prev[2] {
-			result++
-		}
+		if value > prev[2] { result1++ }
+		if value > prev[0] { result2++ }
 		prev[0] = prev[1]
 		prev[1] = prev[2]
 		prev[2] = value
 	}
-	fmt.Println(result)
+	fmt.Println(result1)
+	fmt.Println(result2)
 }
--- d1.java
+++ d1.java
@@ -1,13 +1,17 @@
 public class d1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] prev = new int[]{scanner.nextInt(), scanner.nextInt(), scanner.nextInt()};
-        int result = 0;
+        int[] prev = new int[] {scanner.nextInt(), scanner.nextInt(), scanner.nextInt()};
+        int result1 = 0, result2 = 0;
+        if (prev[1] > prev[0]) result1++;
+        if (prev[2] > prev[1]) result1++;
         while (scanner.hasNext()) {
             int val = scanner.nextInt();
-            if (val + prev[2] + prev[1] > prev[0] + prev[1] + prev[2]) result++;            
+            if (val > prev[2]) result1++;
+            if (val > prev[0]) result2++;
             prev[0] = prev[1]; prev[1] = prev[2]; prev[2] = val;
         }
-        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
     }
 }
