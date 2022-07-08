ecbc6d238def5d62cf07f9bdfbe31518eb680283
pin2t <pin2t@mail.ru>
2022-07-08 16:09:57 +0500

    sinplify

--- d24.java
+++ d24.java
@@ -19,47 +19,44 @@ public class d24 {
         }
         var seen = new ArrayList<Set<Integer>>();
         for (int i = 0; i < programs.size(); i++) seen.add(new HashSet<>());
-        out.println(inputsToZero(programs, new int[] {9, 8, 7, 6, 5, 4, 3, 2, 1}, new Stack<Integer>(), seen, 0)
-                .stream().map(Object::toString).collect(Collectors.joining()));
+        run(programs, new int[] {9, 8, 7, 6, 5, 4, 3, 2, 1}, new Stack<Integer>(), seen, 0);
         for (int i = 0; i < programs.size(); i++) seen.get(i).clear();
-        out.println(inputsToZero(programs, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}, new Stack<Integer>(), seen, 0)
-                .stream().map(Object::toString).collect(Collectors.joining()));
+        run(programs, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}, new Stack<Integer>(), seen, 0);
     }
 
     static int run(List<Instruction> program, int w, int z) {
-        var data = new HashMap<String, Integer>();
-        data.put("w", w);
-        data.put("z", z);
-        data.put("x", 0);
-        data.put("y", 0);
+        var regs = new HashMap<String, Integer>();
+        regs.put("w", w);
+        regs.put("z", z);
+        regs.put("x", 0);
+        regs.put("y", 0);
         for (var i : program) {
-            var b = data.containsKey(i.arg2()) ? data.get(i.arg2()) : Integer.parseInt(i.arg2());
+            var b = regs.containsKey(i.arg2()) ? regs.get(i.arg2()) : Integer.parseInt(i.arg2());
             switch (i.op()) {
-                case "add": data.put(i.arg1(), data.get(i.arg1()) + b); break;
-                case "mul": data.put(i.arg1(), data.get(i.arg1()) * b); break;
-                case "div": data.put(i.arg1(), data.get(i.arg1()) / b); break;
-                case "mod": data.put(i.arg1(), data.get(i.arg1()) % b); break;
-                case "eql": data.put(i.arg1(), data.get(i.arg1()).equals(b) ? 1 : 0); break;
+                case "add": regs.put(i.arg1(), regs.get(i.arg1()) + b); break;
+                case "mul": regs.put(i.arg1(), regs.get(i.arg1()) * b); break;
+                case "div": regs.put(i.arg1(), regs.get(i.arg1()) / b); break;
+                case "mod": regs.put(i.arg1(), regs.get(i.arg1()) % b); break;
+                case "eql": regs.put(i.arg1(), regs.get(i.arg1()).equals(b) ? 1 : 0); break;
             }
         }
-        return data.get("z");
+        return regs.get("z");
     }
 
-    static List<Integer> inputsToZero(List<List<Instruction>> programs, int[] inputs, Stack<Integer> stack, List<Set<Integer>> seen, int z) {
+    static void run(List<List<Instruction>> programs, int[] inputs, Stack<Integer> stack, List<Set<Integer>> seen, int z) {
         var i = stack.size();
-        if (i >= programs.size())
-            return z == 0 ? (List<Integer>) stack.clone() : Collections.emptyList();
-        if (seen.get(i).contains(z)) return Collections.emptyList();
+        if (i >= programs.size() || seen.get(i).contains(z)) return;
         seen.get(i).add(z);
         for (var w : inputs) {
             var zz = run(programs.get(i), w, z);
             stack.push(w);
-            var result = inputsToZero(programs, inputs, stack, seen, zz);
+            if (zz == 0 && stack.size() >= programs.size()) {
+                out.println(stack.stream().map(Object::toString).collect(Collectors.joining()));
+                return;
+            }
+            run(programs, inputs, stack, seen, zz);
             stack.pop();
-            if (!result.isEmpty())
-                return result;
         }
-        return Collections.emptyList();
     }
 
     record Instruction (String op, String arg1, String arg2) {
