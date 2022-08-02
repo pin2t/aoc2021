ce477c3d60215279b69a3370532750dbd08c26ab
pin2t <pin2t@mail.ru>
2022-08-02 20:27:28 +0400

    day 3 ruby second part

--- 3.rb
+++ 3.rb
@@ -1,10 +1,25 @@
 #!ruby
 values = []
 $stdin.each_line do |line| values.append(line.strip) end
-gamma = "000000000000"
-epsilon = "000000000000"
-(0..11).each do |bit|
+gamma = (0..11).map do |bit|
     ones = values.filter do |v| v[bit] == "1" end.count
-    ones > values.count / 2 ? gamma[bit] = "1" : epsilon[bit] = "1"
+    ones > values.count / 2 ? "1" : "0"
+end.inject(&:+).to_i(2)
+epsilon = (0..11).map do |bit|
+    ones = values.filter do |v| v[bit] == "1" end.count
+    ones <= values.count / 2 ? "1" : "0"
+end.inject(&:+).to_i(2)
+puts(gamma * epsilon)
+def filter(values, less)
+    (0..11).each do |bit|
+        ones = values.filter do |v| v[bit] == "1" end
+        zeros = values.filter do |v| v[bit] == "0" end
+        if less
+            values = (zeros.count <= ones.count) ? zeros : ones
+        else
+            values = (zeros.count > ones.count) ? zeros : ones
+        end
+        if values.count == 1 then return values[0].to_i(2) end
+    end
 end
-puts(gamma.to_i(2) * epsilon.to_i(2))
+puts(filter(values, false) * filter(values, true))
