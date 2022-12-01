2cefaad47a3419aecb251a088b0d0f7ccd620a85
pin2t <pin2t@mail.ru>
2022-12-01 08:05:10 +0300

    ...

--- 18.go
+++ 18.go
@@ -3,13 +3,19 @@ package main
 import (
 	"bufio"
 	"os"
+	"regexp"
 )
 
 type number interface {
 	magnitude() int
 }
 
+type child struct {
+	parent *number
+}
+
 type regular struct {
+	child
 	value int
 }
 
@@ -18,6 +24,7 @@ func (r regular) magnitude() int {
 }
 
 type pair struct {
+	child
 	left, right *number
 }
 
@@ -25,7 +32,53 @@ func (p pair) magnitude() int {
 	return 3*(*p.left).magnitude() + 2*(*p.right).magnitude()
 }
 
+type stringsStack []string
+
+func (s stringsStack) push(v string) stringsStack {
+	return append(s, v)
+}
+
+func (s stringsStack) pop() (stringsStack, string) {
+	l := len(s)
+	return s[:l-1], s[l-1]
+}
+
+func (s stringsStack) peek() string {
+	return s[len(s)-1]
+}
+
+type numbersStack []*number
+
+func (s numbersStack) push(v *number) numbersStack {
+	return append(s, v)
+}
+
+func (s numbersStack) pop() (numbersStack, *number) {
+	l := len(s)
+	return s[:l-1], s[l-1]
+}
+
 func parse(s string) number {
+	stack := stringsStack{}
+	out := numbersStack{}
+	pattern := regexp.MustCompile("\\[|]|,|\\d+)")
+	tokens := pattern.FindAllString(s, 0)
+	for _, token := range tokens {
+		switch token {
+		case "[":
+			stack.push(token)
+		case "]":
+			for stack.peek() != "[" {
+				var left, right *number
+				out, right = out.pop()
+				out, left = out.pop()
+				n := &pair{left, right, nil}
+				left.parent = n
+				right.parent = n
+				out = out.push(n)
+			}
+		}
+	}
 	return regular{0}
 }
 
